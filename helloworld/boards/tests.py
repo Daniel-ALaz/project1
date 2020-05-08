4826ca12f605875f8aa6951204679cdf629c7844
Daniel Aguilera <dani.aguilera87@gmail.com>
2020-05-08 10:23:27 +0200

    test

--- helloworld/boards/tests.py
+++ helloworld/boards/tests.py
@@ -8,6 +8,7 @@ from .views import home, board_topics
 from django.contrib.auth.models import User
 from .views import new_topic
 from .forms import NewTopicForm
+from .views import topic_posts
 
 # Create your tests here.
 
@@ -135,4 +136,20 @@ class NewTopicTests(TestCase):
         response = self.client.get(url)
         form = response.context.get('form')
         self.assertIsInstance(form, NewTopicForm)
+
+class TopicPostsTests(TestCase):
+    def setUp(self):
+        board = Board.objects.create(name='Django', description='Django board.')
+        user = User.objects.create_user(username='john', email='john@doe.com', password='123')
+        topic = Topic.objects.create(subject='Hello, world', board=board, starter=user)
+        Post.objects.create(message='Lorem ipsum dolor sit amet', topic=topic, created_by=user)
+        url = reverse('topic_posts', kwargs={'pk': board.pk, 'topic_pk': topic.pk})
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/boards/1/topics/1/')
+        self.assertEquals(view.func, topic_posts)
 
